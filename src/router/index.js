ac63247ab0bacbb21e755dbb5b98800fcabd13ab
taufikraden29 <taufikraden29@gmail.com>
2024-08-14 13:51:45 +0700

    fix navigator

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../pages/Home";
+
+const Stack = createNativeStackNavigator();
 
 const Router = () => {
 	return (
 		<Stack.Navigator>
-			<Stack.Screen name="Home" component={HomeScreen} />
+			<Stack.Screen name="Home" component={Home} />
 		</Stack.Navigator>
 	);
 };
